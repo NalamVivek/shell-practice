0fa7ea906eb5250c87ba992f5fc5d31108b79e1b
Vivek Vardhan <nalamvivekvardhan@gmail.com>
2025-09-20 12:38:34 +0530

    Code for runtime conversations

---
+++ 04-conversations_runtime.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+personA=$1
+person2=$2
+
+echo "This is a story of two persons"
+echo "One day I met $1 and he is a smart guy."
+echo "He's married to beautiful $2"
+echo "Both $1 & $2 lived happily together!"
