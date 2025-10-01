b53171408bc9d5ec4bef318115f39dd86190641d
Vivek Vardhan <nalamvivekvardhan@gmail.com>
2025-10-01 12:46:12 +0530

    variable passing as argument

---
+++ variable2.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+Script_Name=$0
+Scirpt_Path=$PWD
+UserID=$USER 
+
+NAME=$1
+
+echo "Script name is $Script_Name"
+echo "Script path is ${Script_path}"
+echo "Script is ran by user $UserID"
+echo "Script is written by $NAME"
