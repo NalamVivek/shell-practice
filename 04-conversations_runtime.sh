c2891ff220166b1979d0503cf085648034f72579
NalamVivek <nalamvivekvardhan@gmail.com>
2025-10-01 12:03:35 +0530

    Delete 04-conversations_runtime.sh

--- 04-conversations_runtime.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-personA=$1
-person2=$2
-
-echo "This is a story of two persons"
-echo "One day I met $1 and he is a smart guy."
-echo "He's married to beautiful $2"
-echo "Both $1 & $2 lived happily together!"
